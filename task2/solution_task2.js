b0dc926106229bbd32efd33e526aff5f531e252d
Hovhannes Sahakyan <syanhovhannes@gmail.com>
2020-03-27 11:07:17 +0400

    feat: [add] task 2

---
+++ task2/solution_task2.js
@@ -0,0 +1,15 @@
+/**
+ * NO SOLUTION IS PROVIDED. TRY IT YOURSELF
+ *
+ * HINT: you can save the result state in class itself. and then return this.result in getValue().
+ *
+ * function Calculator(initialValue) {
+ *   ....
+ *   if(initialValue == null) {
+ *     this.result = 0;
+ *   } else {
+ *     this.result = initialValue;
+ *   }
+ *   ....
+ * }
+ * */
