299d8e73ecce9583a326ac1335164fec588992bc
Hovhannes Sahakyan <syanhovhannes@gmail.com>
2020-03-27 12:29:07 +0400

    feat: [add] task 4

---
+++ task4/solution_task4.js
@@ -0,0 +1,7 @@
+/**
+ * NO SOLUTION IS PROVIDED. TRY IT YOURSELF
+ *
+ * HINT 1: functions from Array come from Array.prototype and functions from
+ * MyArray come from MyArray.prototype.
+ *
+ * */
